0f79507df9daafb1590aea8dcde1aa39205ffeae
RFNoC <rj.geng@gmail.com>
2023-02-03 12:19:11 -0800

    DOM II - object 5 lecture complete

--- index.js
+++ index.js
@@ -1,72 +1,21 @@
-window.onload = function (event) {
-    console.log(`an event happened, of type ${event.type}`)
+function listener(event) {
+    console.log(`
+    event passing through ${event.currentTarget.nodeName || 'Window'}
+    target --> ${event.target.nodeName}
+    `);
 }
-window.addEventListener('load', event => {
-    console.log( `${event.type} happened`);
-})
-
-const theButton = document.querySelector('#theButton');
-
-theButton.onclick = function (event){
-    console.log( `${event.type} happened`);
-}
-theButton.addEventListener('click', event => {
-    console.log(`you ${event.type}ed on the ${event.target.nodeType}`);
-})
-theButton.addEventListener('click', event => {
-    console.log(`about ton change the DOM`);
-    event.target.style.backgroundColor = 'red';
-    document.body.style.backgroundColor = 'blue';
-})
 
 
-/* Use this sample as a quide */
-let sampleButton = document.querySelector('.sampleButton');
-sampleButton.addEventListener('click', (event) => {
-  alert('You Clicked the Sample Button!')
-})
+window.addEventListener('click', listener);
+theDiv.addEventListener('click', listener);
+document.addEventListener('click', listener);
 
-/* NOTE: DO NOT change any HTML or CSS in this exercise, refer to the HTML for the class names of the elements */
+document.body.addEventListener('click', e => e.stopImmediatePropagation());
+//document.body.addEventListener('click', e => e.stopPropagation());
 
-/* Step One: Select the container and button elements */
+document.body.addEventListener('click', listener);
 
+//document.body.addEventListener('click', listener, {capture: true});
 
-
-/* Step Two: Use the method addEventListener on the button element, listening for a 'click' event */
-
-
-
-/* Step Three: Using the 'event' object in the callback, Change the background color of the button */
-
-
-
-/* Step Four: Click on the button, does it work? */
-
-
-
-/* Step Five: Use the method addEventListener on the container element, listening for a 'mouseenter' event */
-
-
-
-/* Step Six: Using the 'event' object in the callback, Change the background color and the border color of the container */
-
-
-
-/* Step Seven: Reverse the color changes to the container on a 'mouseleave' event */
-
-
-
-/* Step Eight: Finish the section in TK on stopPropagation */
-
-
-
-/* Step Nine: Create a click event on the container, it can do whatever you want it to, but make it visible */
-
-
-
-/* Step Ten: Click the button, notice how the container click event also triggers? */
-
-/* Step Eleven: Add stopPropagation to the button click handler */
-
-/* Step Twelve: Click the button now, the click should not propagate to the conainer. */
-/* All Done! */
+theButton.addEventListener('click', e => console.log('click!'));
+theButton.addEventListener('click', listener);
